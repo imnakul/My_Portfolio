63bd04399d25d2df78c479b7dc85a3e34c9c3e31
Nakul Srivastava <imnakul44@gmail.com>
2024-12-06 11:10:33 +0530

    Added copy button in Quotes

--- src/components/Quotes.jsx
+++ src/components/Quotes.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useRef } from 'react'
 
 const QuoteSection = () => {
    const [quote, setQuote] = useState('Loading an inspiring quote...')
+   const [copied, setCopied] = useState(false)
    const quoteRef = useRef()
 
    const fetchQuote = async () => {
@@ -12,7 +13,7 @@ const QuoteSection = () => {
             {
                method: 'GET',
                headers: {
-                  'X-Api-Key': '',
+                  'X-Api-Key': '+aSb1w8rKyV1bHIzo9rwiw==O5XsatrRBPBWK7wD',
                   //   +aSb1w8rKyV1bHIzo9rwiw==O5XsatrRBPBWK7wD
                   'Content-Type': 'application/json',
                },
@@ -34,6 +35,14 @@ const QuoteSection = () => {
       }
    }
 
+   const handleCopy = () => {
+      // Copies the dynamically fetched quote
+      navigator.clipboard.writeText(quote).then(() => {
+         setCopied(true)
+         setTimeout(() => setCopied(false), 1500) // Hide "Copied!" popup after 1.5 seconds
+      })
+   }
+
    useEffect(() => {
       //   const observer = new IntersectionObserver((entries) => {
       //      if (entries[0].isIntersecting) {
@@ -50,12 +59,28 @@ const QuoteSection = () => {
    return (
       <div
          ref={quoteRef}
-         className='max-w-xs xl:max-w-2xl text-center text-black text-xl xl:text-2xl font-extra-light font-playwrite-hr mx-auto  rounded-lg shadow-lg px-4 py-2 border-2 border-cyan-300 animate-staticrainbow bg-[length:200%_200%] bg-clip-border  drop-shadow-lg'
+         className='relative max-w-xs xl:max-w-2xl text-center text-black text-xl xl:text-2xl font-extra-light font-playwrite-hr mx-auto  rounded-lg shadow-lg px-4 py-2 border-2 border-cyan-300 animate-staticrainbow bg-[length:200%_200%] bg-clip-border drop-shadow-lg pr-10'
       >
          {/* text-shadow */}
          {/* bg-gradient-to-r from-blue-900 to-purple-900   */}
          {/* bg-gradient-to-r from-purple-950 via-gray-900 to-purple-950 animate-gradient-x */}
          {quote}
+
+         {/* Copy Button */}
+         <button
+            onClick={handleCopy}
+            className='absolute bottom-2 right-2 bg-transparent hover:opacity-70 text-white p-0 mb-1 rounded-full shadow-lg focus:outline-none border-1 border-black'
+            title='Copy text'
+         >
+            <img src='assets/copy2.png' alt='copyIcon' className='w-4 h-4' />
+         </button>
+
+         {/* Copied Popup */}
+         {copied && (
+            <div className='absolute top-16 right-2 bg-white text-black text-xs px-2 py-2 rounded-md shadow-md'>
+               Quote Copied!
+            </div>
+         )}
       </div>
    )
 }
